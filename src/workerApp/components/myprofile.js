e73a45400cdb50ed23bcf3891260cb66106e4c4a
NIdev866 <nidev866@gmail.com>
2017-09-29 08:19:40 +0100

    new pic lib

--- src/workerApp/components/myprofile.js
+++ src/workerApp/components/myprofile.js
@@ -5,28 +5,74 @@ import styles from './form_material_styles'
 import renderField from '../renderField'
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
-
-
 import TextField from 'material-ui/TextField'
 import {blue500, grey400} from 'material-ui/styles/colors'
 
+import AvatarCropper from "react-avatar-cropper";
+import ReactDom from "react-dom";
 
 
-import CropperJS from 'react-cropperjs';
+
+var FileUpload = React.createClass({
+
+  handleFile: function(e) {
+    var reader = new FileReader();
+    var file = e.target.files[0];
+
+    if (!file) return;
+
+    reader.onload = function(img) {
+      ReactDom.findDOMNode(this.refs.in).value = '';
+      this.props.handleFileChange(img.target.result);
+    }.bind(this);
+    reader.readAsDataURL(file);
+  },
+
+  render: function() {
+    return (
+      <input ref="in" type="file" accept="image/*" onChange={this.handleFile} />
+    );
+  }
+});
 
 
 
 class Myprofile extends Component{
   constructor(props){
     super(props)
-    this._crop = this._crop.bind(this)
+    this.handleFileChange = this.handleFileChange.bind(this)
+    this.handleCrop = this.handleCrop.bind(this)
+    this.handleRequestHide = this.handleRequestHide.bind(this)
+    this.state = {
+      cropperOpen: false,
+      img: null,
+      croppedImg: "http://www.fillmurray.com/400/400"
+    }
   }
 
 
-  _crop(){
-    // image in dataUrl 
-    console.log(this.refs.cropper.getCroppedCanvas().toDataURL());
+
+  handleFileChange(dataURI) {
+    this.setState({
+      img: dataURI,
+      croppedImg: this.state.croppedImg,
+      cropperOpen: true
+    });
   }
+
+  handleCrop(dataURI) {
+    this.setState({
+      cropperOpen: false,
+      img: null,
+      croppedImg: dataURI
+    });
+  }
+  handleRequestHide() {
+    this.setState({
+      cropperOpen: false
+    });
+  }
+
 
 
   render(){
@@ -53,21 +99,191 @@ class Myprofile extends Component{
 
 
 
-
-      <CropperJS
-        ref='cropper'
-        src='http://i.imgur.com/n483ZwJ.jpg'
-        style={{height: 400, width: '100%'}}
-        // Cropper.js options 
-        aspectRatio={16 / 9}
-        guides={false}
-        crop={this._crop.bind(this)} />
+          <FileUpload handleFileChange={this.handleFileChange} />
 
 
-https://github.com/blueimp/JavaScript-Canvas-to-Blob
+        {this.state.cropperOpen &&
+          <AvatarCropper
+            onRequestHide={this.handleRequestHide}
+            cropperOpen={this.state.cropperOpen}
+            onCrop={this.handleCrop}
+            image={this.state.img}
+            width={400}
+            height={400}
+          />
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+EXAMPLEEE
+import React from "react";
+import ReactDom from "react-dom";
+import AvatarCropper from "../../lib";
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      cropperOpen: false,
+      img: null,
+      croppedImg: "http://www.fillmurray.com/400/400"
+    };
+  },
+  handleFileChange: function(dataURI) {
+    this.setState({
+      img: dataURI,
+      croppedImg: this.state.croppedImg,
+      cropperOpen: true
+    });
+  },
+  handleCrop: function(dataURI) {
+    this.setState({
+      cropperOpen: false,
+      img: null,
+      croppedImg: dataURI
+    });
+  },
+  handleRequestHide: function() {
+    this.setState({
+      cropperOpen: false
+    });
+  },
+  render () {
+    return (
+      <div>
+        <div className="avatar-photo">
+          <FileUpload handleFileChange={this.handleFileChange} />
+          <div className="avatar-edit">
+            <span>Click to Pick Avatar</span>
+            <i className="fa fa-camera"></i>
+          </div>
+          <img src={this.state.croppedImg} />
+        </div>
+        {this.state.cropperOpen &&
+          <AvatarCropper
+            onRequestHide={this.handleRequestHide}
+            cropperOpen={this.state.cropperOpen}
+            onCrop={this.handleCrop}
+            image={this.state.img}
+            width={400}
+            height={400}
+          />
+        }
+      </div>
+    );
+  }
+});
+
+var FileUpload = React.createClass({
+
+  handleFile: function(e) {
+    var reader = new FileReader();
+    var file = e.target.files[0];
+
+    if (!file) return;
+
+    reader.onload = function(img) {
+      ReactDom.findDOMNode(this.refs.in).value = '';
+      this.props.handleFileChange(img.target.result);
+    }.bind(this);
+    reader.readAsDataURL(file);
+  },
+
+  render: function() {
+    return (
+      <input ref="in" type="file" accept="image/*" onChange={this.handleFile} />
+    );
+  }
+});
+
+ReactDom.render(<App />, document.getElementById("content"));
+EXAMPLEE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/*https://github.com/blueimp/JavaScript-Canvas-to-Blob
 https://blueimp.github.io/JavaScript-Canvas-to-Blob/test/
 https://www.npmjs.com/package/react-cropperjs
 https://www.kurzor.co.uk/blog/28-uploading-and-resizing-images-with-reactjs
+https://github.com/DropsOfSerenity/react-avatar-cropper*/}
 
 
 {/*                  <RaisedButton
